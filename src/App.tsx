017af034eff26cdff6c385c5b6b8d4389e7b1ad3
Jordan Suber <jordansuber@gmail.com>
2024-05-21 17:11:31 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import clubs_2 from './img/svg_playing_cards/fronts/optimized-clubs_2.svg';
+
+function App() {
+  return (
+    <div className="App">
+        <img src={clubs_2} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
